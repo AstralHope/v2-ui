dcf294d678bed04fd400b15dd038eba7f0b1b5ad
sprov <sprov065@gmail.com>
2019-08-13 12:10:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -114,7 +114,7 @@ install_v2-ui() {
     echo -e ""
     echo -e "如果是更新面板，则按你之前的方式访问面板"
     echo -e ""
-    curl -o /usr/bin/v2-ui --silent https://raw.githubusercontent.com/sprov065/v2-ui/master/install.sh
+    curl -o /usr/bin/v2-ui --silent https://raw.githubusercontent.com/sprov065/v2-ui/master/v2-ui.sh
     chmod +x /usr/bin/v2-ui
     echo -e "v2-ui 管理脚本使用方法: "
     echo -e "----------------------------------------------"
