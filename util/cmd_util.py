e8174e080e7911141f31972de548a0914c3592a5
sprov <sprov065@gmail.com>
2020-10-02 21:58:16 +0800

    5.4.4
     - 修复启动一段时间后所有任务失效的问题

--- util/cmd_util.py
+++ util/cmd_util.py
@@ -17,14 +17,20 @@ def exec_cmd(cmd, timeout=10) -> Tuple[str, int]:
         task = Timer(timeout, close_task)
         task.start()
         encoding = 'gbk' if __is_windows else 'utf-8'
-        with subprocess.Popen(['sh', '-c', cmd], shell=False, stdout=subprocess.PIPE,
-                              stderr=subprocess.STDOUT, encoding=encoding) as process:
-            p = process
-            code = process.wait(timeout)
-            result = process.stdout.read()
-            p = None
-            task.cancel()
-        return result, code
+        process = subprocess.Popen(['sh', '-c', cmd], shell=False, stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT, encoding=encoding)
+        p = process
+        code = process.wait(timeout)
+        lines = []
+        p = None
+        task.cancel()
+        if code == 0:
+            while True:
+                line = process.stdout.readline()
+                if not line:
+                    break
+                lines.append(line)
+        return ''.join(lines), code
     except Exception as e:
         if not __is_windows:
             logging.warning(f'execute \"{cmd}\" failed: {e}')
--- util/v2_jobs.py
+++ util/v2_jobs.py
@@ -11,7 +11,7 @@ from util import config, v2_util
 from util.schedule_util import schedule_job
 from v2ray.models import Inbound
 
-__lock = threading.Lock()
+# __lock = threading.Lock()
 __v2_config_changed = True
 
 
@@ -28,28 +28,28 @@ def v2_config_change(func):
 def check_v2_config_job():
     global __v2_config_changed
     if __v2_config_changed:
-        with __lock:
-            v2_config = v2_util.gen_v2_config_from_db()
-            v2_util.write_v2_config(v2_config)
-            __v2_config_changed = False
+        # with __lock:
+        v2_config = v2_util.gen_v2_config_from_db()
+        v2_util.write_v2_config(v2_config)
+        __v2_config_changed = False
 
 
 def traffic_job():
-    with __lock:
-        if not v2_util.is_running():
+    # with __lock:
+    if not v2_util.is_running():
+        return
+    try:
+        traffics = v2_util.get_inbounds_traffic()
+        if not traffics:
             return
-        try:
-            traffics = v2_util.get_inbounds_traffic()
-            if not traffics:
-                return
-            for traffic in traffics:
-                upload = int(traffic.get('uplink', 0))
-                download = int(traffic.get('downlink', 0))
-                tag = traffic['tag']
-                Inbound.query.filter_by(tag=tag).update({'up': Inbound.up + upload, 'down': Inbound.down + download})
-            db.session.commit()
-        except Exception as e:
-            logging.warning(f'traffic job error: {e}')
+        for traffic in traffics:
+            upload = int(traffic.get('uplink', 0))
+            download = int(traffic.get('downlink', 0))
+            tag = traffic['tag']
+            Inbound.query.filter_by(tag=tag).update({'up': Inbound.up + upload, 'down': Inbound.down + download})
+        db.session.commit()
+    except Exception as e:
+        logging.warning(f'traffic job error: {e}')
 
 
 def reset_traffic_job():
@@ -84,15 +84,10 @@ def check_v2ay_alive_job():
         v2_util.restart(True)
 
 
-def check_v2ray_version_job():
-    requests.get('')
-
-
 def init():
     schedule_job(check_v2_config_job, config.get_v2_config_check_interval())
     schedule_job(traffic_job, config.get_traffic_job_interval())
     schedule_job(check_v2ay_alive_job, 40)
-    schedule_job(check_v2ray_version_job, 1800)
     # reset_day = config.get_reset_traffic_day()
     # if reset_day <= 0:
     #     return
